73c9fdeb0fd2cfc48996afd2ce2872f13f7ef5bf
Ahmed Gasser <ahmed1.gasser@gmail.com>
2024-10-10 01:15:06 +0300

    added tourist model

---
+++ server/models/tourist.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const touristSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    mobile_number:{
+        type:Number,
+        required:true
+    },
+    nationality:{
+        type:String,
+        required:true
+    },
+    date_of_birth:{
+        type:Date,
+        required:true
+    },
+    occupation:{ //enum?
+        type:String,
+        required:true
+    }, 
+
+});
+
+const Tourist = mongoose.model("Tourist", touristSchema); //store in table "Tourist"
+
+module.exports =  Tourist; //export for use in other files
