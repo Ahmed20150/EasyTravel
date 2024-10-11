fd40e6872f3111695c9e777dfeb0ae616c3392e7
sp4rkii <ahmed.labib@student.guc.edu.eg>
2024-10-12 01:10:23 +0300

    working add admin plus touristGoverner

--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Admin = require('../models/admin.model');
 const TourismGoverner = require('../models/tourismGoverner.model');
-
+router.use(express.json());
 
 //Add Tourism Governer
 router.post('/add-tourismGoverner', async (req, res) => {
@@ -32,32 +32,33 @@ router.post('/add-tourismGoverner', async (req, res) => {
 
 //Add Admin
 router.post('/add-admin', async (req, res) => {
-        const { username, password } = req.body;
-    
-        // Check if username and password are provided
-        if (!username || !password) {
-            return res.status(400).json({ message: 'Username and Password are required.' });
+    const { username, password } = req.body;
+
+    // Check if username and password are provided
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and Password are required.' });
+    }
+
+    try {
+        // Check if the username already exists
+        const existingUser = await Admin.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username already exists. Please choose a different one.' });
         }
-    
-        try {
-            // Check if the username already exists
-            const existingUser = await Admin.findOne({ username });
-            if (existingUser) {
-                return res.status(400).json({ message: 'Username already exists. Please choose a different one.' });
-            }
-    
-            // Create a new Admin with the unique username
-            const newAdmin = await Admin.create(req.body);
-    
-            // Save the new Admin to the database
-            await newAdmin.save();
-    
-            // Return success message
-            res.status(201).json({ message: 'Admin account added successfully.' });
-        } catch (error) {
-            // Handle any errors that occur during the process
-            res.status(500).json({ message: 'Error adding Admin account', error });
-        }});
-    
+
+        // Create a new Admin with the unique username
+        const newAdmin = await Admin.create(req.body);
+
+        // Save the new Admin to the database
+        await newAdmin.save();
+
+        // Return success message
+        res.status(201).json({ message: 'Admin account added successfully.' });
+    } catch (error) {
+        // Handle any errors that occur during the process
+        res.status(500).json({ message: 'Error adding Admin account', error });
+    }
+});
+
 
 module.exports = router;
