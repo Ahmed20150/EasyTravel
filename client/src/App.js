5cc9e38574945465c1d995fed29af1bc0d0cbb99
sp4rkii <ahmed.labib@student.guc.edu.eg>
2024-10-12 01:19:54 +0300

    idk

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import TempHomePage from './pages/tempHomePage';
+
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />}/>
+            <Route path="/signUp"element={<SignUpForm />}/>
+            <Route path="/login"element={<LoginForm />}/>
+            <Route path="/forgotPassword"element={<ForgotPassword />}/>
+            <Route path="/changePassword"element={<ChangePassword />}/>
+            <Route path="/home"element={<TempHomePage />}/>
+            <Route path="/admin"/>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
