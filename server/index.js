754ab8d927be753c20e810dc78b138bf0490a2e8
sp4rkii <ahmed.labib@student.guc.edu.eg>
2024-10-11 23:59:47 +0300

    useless commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-const express= require("express");
+const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 const Tourist = require("./models/tourist.model.js");
+const adminRoutes = require('./routes/admin.routes.js');
+
+//connect admin.routes.js to index.js
+app.use('/admin', adminRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -13,7 +17,8 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-mongoose.connect("mongodb+srv://ahmed1gasser:jxaauvDrMDrxvUQS@acl.05st6.mongodb.net/?retryWrites=true&w=majority&appName=ACL").then(() =>{
+mongoose.connect("mongodb+srv://ahmed1gasser:jxaauvDrMDrxvUQS@acl.05st6.mongodb.net/?retryWrites=true&w=majority&appName=ACL").then(() => {
+
     console.log("Connected to the database!");
 }).catch((err) => {
     console.log("Cannot connect to the database!", err);
@@ -22,22 +27,22 @@ mongoose.connect("mongodb+srv://ahmed1gasser:jxaauvDrMDrxvUQS@acl.05st6.mongodb.
 
 
 //Create Tourist
-app.post('/api/tourists', async (req, res) => { 
+app.post('/api/tourists', async (req, res) => {
     try {
         // const tourist = await Tourist.create(req.body);
-        const tourist = await Tourist.create({"username":"ahmed20150", "email":"a@gmail.com", "password":"123456", "mobile_number":123456789, "nationality":"Egyptian", "date_of_birth":"2000-01-01", "occupation":"student"});
+        const tourist = await Tourist.create({ "username": "ahmed20150", "email": "a@gmail.com", "password": "123456", "mobile_number": 123456789, "nationality": "Egyptian", "date_of_birth": "2000-01-01", "occupation": "student" });
         res.status(200).json(tourist);
     } catch (err) {
-        res.status(500).json({message: err.message});
+        res.status(500).json({ message: err.message });
     }
 });
 
 //getting all tourists
-app.get('/api/tourists', async (req, res) => { 
+app.get('/api/tourists', async (req, res) => {
     try {
         const tourists = await Tourist.find({});
         res.status(200).json(tourists);
     } catch (err) {
-        res.status(500).json({message: err.message});
+        res.status(500).json({ message: err.message });
     }
 });
--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -1,58 +1,59 @@
-const express= require("express");
+const express = require("express");
 const router = express.Router();
 const Admin = require('../models/admin.model');
-const TourismGoverner=require('../models/tourismGoverner.model');
+const TourismGoverner = require('../models/tourismGoverner.model');
 
 
 //Add Tourism Governer
-router.post('/add-tourismGoverner',async(req,res)=>{
-    const { username,password }=req.body;
+router.post('/add-tourismGoverner', async (req, res) => {
+    const { username, password } = req.body;
 
-    if (!username||!password){
-        return res.status(400).json({message:'Username and Password Required'});
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and Password Required' });
     }
 
-    try{
-        const NonUniqueUser=await TourismGoverner.findOne({username});
-        if(NonUniqueUser){
-        return res.status(400).json({message:'Username already exists'});
+    try {
+        const NonUniqueUser = await TourismGoverner.findOne({ username });
+        if (NonUniqueUser) {
+            return res.status(400).json({ message: 'Username already exists' });
         }
-        const newTourismGoverner=new TourismGoverner({
+        const newTourismGoverner = new TourismGoverner({
             username,
             password
         });
         await TourismGoverner.save();
-        res.status(201).json({message:'Tourism Governer Added Successfully'});
+        res.status(201).json({ message: 'Tourism Governer Added Successfully' });
     }
-    catch(error){
-        res.status(500).json({message:'Error Creating new Tourism Governer Account',error});
+    catch (error) {
+        res.status(500).json({ message: 'Error Creating new Tourism Governer Account', error });
     }
-    });
+});
 
 
 //Add Admin
-router.post('/add-admin',async(req,res)=>{
-    const { username,password }=req.body;
+router.post('/add-admin', async (req, res) => {
+    const { username, password } = req.body;
 
-    if (!username||!password){
-        return res.status(400).json({message:'Username and Password Required'});
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and Password Required' });
     }
 
-    try{
-        const NonUniqueUser=await TourismGoverner.findOne({username});
-        if(NonUniqueUser){
-        return res.status(400).json({message:'Username already exists'});
+    try {
+        const NonUniqueUser = await TourismGoverner.findOne({ username });
+        if (NonUniqueUser) {
+            return res.status(400).json({ message: 'Username already exists' });
         }
-        const newTourismGoverner=new TourismGoverner({
+        const newTourismGoverner = new TourismGoverner({
             username,
             password
         });
         await TourismGoverner.save();
-        res.status(201).json({message:'Admin Account Added Successfully'});
+        res.status(201).json({ message: 'Admin Account Added Successfully' });
     }
-    catch(error){
-        res.status(500).json({message:'Error Creating new Admin Account',error});
+    catch (error) {
+        res.status(500).json({ message: 'Error Creating new Admin Account', error });
     }
-    });
+});
 
 
+module.exports = router;
