fce9d1e8b1f3b7e327705fed3d21d6e6e800a8b9
amrkhaled04 <105929016+amrkhaled04@users.noreply.github.com>
2024-10-12 19:02:00 +0200

    updating tour guide

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ import ChangePassword from './components/changePassword';
 import ForgotPasswordForm from './components/ForgotPasswordForm';
 import GeneralSignUpForm from './components/GeneralSignUpForm';
 import LoginForm from './components/LoginForm';
+import CreateProfilePage from './pages/CreateProfilePage';
 import LandingPage from './pages/LandingPage';
 import TempHomePage from './pages/tempHomePage';
+// import EditProfilePage from './pages/';
 
 
 
@@ -21,9 +23,10 @@ function App() {
             <Route path="/changePassword"element={<ChangePassword />}/>
             <Route path="/home"element={<TempHomePage />}/>
             <Route path='/forgotPasswordForm' element={<ForgotPasswordForm />} />
-            <Route path="/create-profile" element={<CreateProfilePage />} />
-            <Route path="/view-profile/:email" element={<ViewProfilePage />} />
-            <Route path="/edit-profile/:email" element={<EditProfilePage />} />
+            <Route path='/create-profile' element={<CreateProfilePage />} />
+            {/* <Route path="/create-profile" element={<CreateProfilePage />}>
+            <Route path="/view-profile/:email" element={<ViewProfilePage />} /> */}
+            {/* <Route path="/edit-profile/:email" element={<EditProfilePage />} /> */}
           </Routes>
       </BrowserRouter>
     </div>
--- server/index.js
+++ server/index.js
@@ -291,5 +291,5 @@ app.get('/api/tourists', async (req, res) => {
 // });
 
 app.use('/api', tourGuideRoutes);
-app.listen(3000, () => console.log('Server running on port 3000'));
+// app.listen(3000, () => console.log('Server running on port 3000'));
 
