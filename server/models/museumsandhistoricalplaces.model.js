61a740dd03f531a2b90b1bda6261245c1ea0ed7f
Ahmed Gasser <ahmed1.gasser@gmail.com>
2024-10-21 21:36:47 +0300

    added numOfPurchases

--- server/models/museumsandhistoricalplaces.model.js
+++ server/models/museumsandhistoricalplaces.model.js
@@ -1,12 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const museumsAndHistoricalPlacesSchema = new mongoose.Schema({
   name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  description: {
     type: String,
     required: true,
   },
@@ -14,29 +9,17 @@ const museumsAndHistoricalPlacesSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  openingHours: {
-    from: {
-      type: String,
-      required: true,
-    },
-    to: {
-      type: String,
-      required: true,
-    },
+  description: {
+    type: String,
+    required: true,
   },
-  ticketPrices: {
-    foreigner: {
-      type: Number,
-      required: true,
-    },
-    native: {
-      type: Number,
-      required: true,
-    },
-    student: {
-      type: Number,
-      required: true,
-    },
+  openingHours: {
+    type: String,
+    required: true,
+  },
+  ticketPrice: {
+    type: Number,
+    required: true,
   },
   picture: {
     type: String,
@@ -46,11 +29,14 @@ const museumsAndHistoricalPlacesSchema = new mongoose.Schema({
       type: String, // e.g., Monuments, Museums, Religious Sites, Palaces/Castles
     },
   ],
-  numofpurchases:{type: Number, default: 1},
+  numofpurchases: {
+    type: Number,
+    default: 1,
+  },
 });
 
 const MuseumsAndHistoricalPlaces = mongoose.model(
-  "MuseumsAndHistoricalPlaces",
+  'MuseumsAndHistoricalPlaces',
   museumsAndHistoricalPlacesSchema
 );
 
