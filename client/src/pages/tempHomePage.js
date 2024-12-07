d0aa6622bd92ec61e2c80385bbc262bb677b3887
Ahmed Gasser <ahmed1.gasser@gmail.com>
2024-12-07 03:47:24 +0100

    added frontend guidance textfiles, added homecard component, added style file for each individual member + General Style file

--- client/src/pages/tempHomePage.js
+++ client/src/pages/tempHomePage.js
@@ -9,6 +9,7 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import GeneralNavbar from "../components/GeneralNavbar";
 import { Navbar, Button, Card, Footer } from "flowbite-react";
 import { buttonStyle, cardStyle, linkStyle, centerVertically, fadeIn,stepStyle, stepIconStyle, stepTitleStyle, stepDescriptionStyle } from "../styles/gasserStyles"; 
+import HomeCard from "../components/HomeCard";
 
 
 const TempHomePage = () => {
@@ -154,26 +155,13 @@ const TempHomePage = () => {
         <p>Your email: {userEmail}</p> // Display the email for admin users
       )}
       {/* <Button className={buttonStyle} onClick={handleLogout}>Logout</Button> */}
-     
-<div
-  className="relative rounded-lg -skew-x-6 -translate-y-2 -translate-y-6 hover:-translate-y-1 hover:-translate-x-0 hover:skew-x-0 duration-500 w-72 h-44 p-2 bg-neutral-50 card-compact hover:bg-base-200 transition-all duration-200 [box-shadow:12px_12px] hover:[box-shadow:4px_4px]"
->
-  <Link to="/changePassword" className={linkStyle}> 
-  <figure className="w-full h-full">
-    <div
-      alt="change to a img tag"
-      className="bg-neutral-800 text-neutral-50 min-h-full rounded-lg border border-opacity-5"
-    ></div>
-  </figure>
-  <div className="absolute text-neutral-50 bottom-4 left-0 px-4">
-    <span className="font-bold">Change Password</span>
-    <p className="text-sm opacity-60 line-clamp-2">
-      Change your password and keep your account secure!
-    </p>
-  </div>
-  </Link>
-</div>
+    
 
+<HomeCard
+            title="Change Password"
+            description="Change your password and keep your account secure!"
+            linkRoute="/changePassword"
+          />
 
       {userType !== "admin" &&
         notifications.length > 0 &&
@@ -217,24 +205,12 @@ const TempHomePage = () => {
 
       {userType === "tourismGoverner" && (
         <>
-        <div
-  className="relative rounded-lg -skew-x-6 -translate-y-2 -translate-y-6 hover:-translate-y-1 hover:-translate-x-0 hover:skew-x-0 duration-500 w-72 h-44 p-2 bg-neutral-50 card-compact hover:bg-base-200 transition-all duration-200 [box-shadow:12px_12px] hover:[box-shadow:4px_4px]"
->
-  <Link to="/museums" className={linkStyle}> 
-  <figure className="w-full h-full">
-    <div
-      alt="change to a img tag"
-      className="bg-neutral-800 text-neutral-50 min-h-full rounded-lg border border-opacity-5"
-    ></div>
-  </figure>
-  <div className="absolute text-neutral-50 bottom-4 left-0 px-4">
-    <span className="font-bold">Museums & Historical Places</span>
-    <p className="text-sm opacity-60 line-clamp-2">
-      View and Post Museums & Historical Places
-    </p>
-  </div>
-  </Link>
-</div>
+
+          <HomeCard
+            title="Museums & Historical Places"
+            description="View, Post & Manage Museums & Historical Places"
+            linkRoute="/museums"
+          />
 </>
 
       )}
---
+++ client/src/styles/GeneralStyles.js
@@ -0,0 +1,1 @@
+export const buttonStyle = "bg-black text-white font-bold hover:bg-gray-700 transition-colors duration-300";
